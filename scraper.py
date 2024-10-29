003f356bc62d119996b1523fcbc42e20334c0951
Andy Arreola <aarreol6@uci.edu>
2024-10-28 20:18:02 -0700

    nothing

--- scraper.py
+++ scraper.py
@@ -101,18 +101,24 @@ storage = {
 def scraper(url, resp):
 
     #Theory to see if this actually checks whether "content" is empty or small
-    
+    '''
     if len(resp.raw_response.content) < 100:
         return []
-    
     '''
+    # checks if status is not OK
+    if resp.status != 200 or not resp.raw_response or not resp.raw_response.content:
+        return []
 
-    content = resp.raw_response.content.decode('utf-8')
+    content = resp.raw_response.content.decode('utf-8', errors = 'ignore' )
+    # converts HTML to plain text
+    text_content = html.fromstring(content).text_content().lower()
+    
     words = re.findall(r'\b\w+\b', content)
     if len(words) < 100:
         return []
-
+    
     links = extract_next_links(url, resp)
+
     """
     #stores unique pages, disregarding fragements. 
     #can just be put under extract_next_links i believe before appending ? 
